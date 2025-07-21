b782817937619adfaa5586502e35f45e1a8b4b7e
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-07-21 14:52:04 +0530

    New Push

--- controller/newsDetailPage.controller.js
+++ controller/newsDetailPage.controller.js
@@ -1,9 +1,16 @@
-const  { Slug } = require('../models');
+const { Slug } = require('../models');
 
- const getNewsDetailPages = async (req, res) => {
+const getNewsDetailPages = async (req, res) => {
   try {
-    const pages = await Slug.find({ type: "NewsDetailPage" });
-    res.status(200).json({ status: true, data: pages });
+    const pages = await Slug.find({ type: "News", ComponentType: "news-details", status: true, deleteflag: false });
+    if (pages) {
+      return res.status(404).json({
+        status: false,
+        message: 'Data Not Found',
+        data: false
+      })
+    }
+    res.status(200).json({ status: true, message: "News List Found !", data: pages });
   } catch (error) {
     console.error("Error fetching NewsDetailPage pages:", error);
     res.status(500).json({ status: false, message: "Internal server error" });
@@ -11,6 +18,6 @@ const  { Slug } = require('../models');
 };
 
 module.exports
-    = {
-    getNewsDetailPages
-    };
+  = {
+  getNewsDetailPages
+};
