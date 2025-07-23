5ae92a491a1f372405bd97cd5109d965ac3181fc
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-07-23 12:00:15 +0530

    New Push

--- controller/widget.controller.js
+++ controller/widget.controller.js
@@ -33,7 +33,7 @@ const getWidgetData = async (req, res) => {
 
         // Paginated result
         const results = await Slug.find(query)
-            .select('name shortdesc description banner_img path page_id type date')
+            .select('name shortdesc description banner_img path page_id type date widgetType')
             .sort({ createdAt: -1 })
             .skip(skip)
             .limit(parsedLimit);
