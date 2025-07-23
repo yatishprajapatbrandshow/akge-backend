e53eb20d3b422f4c2d775f3d6765061f285e07f7
atul-xt <atul.xtt@gmail.com>
2025-07-23 13:13:42 +0530

    authentication done

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const adminSchema = new mongoose.Schema({
     username: {
@@ -8,7 +11,14 @@ const adminSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "Password is required"],
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error(
+                    "Strong password is required (A-Z, a-z, 1-9, special character) must 8 digit"
+                );
+            }
+        },
     },
     type: {
         type: String,
@@ -16,7 +26,17 @@ const adminSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+        validate(value) {
+            if (!value) {
+                throw new Error("emailId is required");
+            } else if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address");
+            }
+        },
     },
     mobile: {
         type: String,
@@ -32,5 +52,32 @@ const adminSchema = new mongoose.Schema({
         default: true
     }
 }, { timestamps: true, timeseries: true });
+
+adminSchema.methods.getJWT = function () {
+    try {
+        const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+        });
+        return token;
+    } catch (error) {
+        throw new Error("Error while getting jwt token");
+    }
+};
+
+adminSchema.methods.checkBcryptPassword = async function (password) {
+    try {
+        const isPasswordValid = await bcrypt.compare(password, this.password);
+        return isPasswordValid;
+    } catch (error) {
+        throw new Error("Error while checking password");
+    }
+};
+
+adminSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
 module.exports = mongoose.model('Admin', adminSchema);
